0eecd42afca92e13b39de55823becf99860c63f4
N Anand <nanand@xebia.com>
2014-07-28 12:52:05 +0530

    pattern 3

--- pattern/BuilderTest.java
+++ pattern/BuilderTest.java
@@ -4,7 +4,17 @@ public class BuilderTest {
 
 	public static void main(String[] args) {
 	
-		Cake cake = new Cake();
+		//Creating object using Builder pattern in java
+        Cake whiteCake = new Cake.Builder(4, 2.5, 6)
+        					.withSugar(1)
+        					.withButter(0.5)
+        					.withEggs(2)
+        					.withVanila(2)
+        					.build();
+      
+        //Cake is ready to eat :)
+        System.out.println(whiteCake);
+
 	}
 	
 }
